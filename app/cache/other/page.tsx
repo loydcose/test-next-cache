75d6e5dfb7edf2a447afa9f942215ac37408d500
LAPTOP-7IMPE5DI\loydc <loydcose.01@gmail.com>
2024-01-23 16:08:35 +0800

    force cache on client component wouldn't work

--- app/cache/other/page.tsx
+++ app/cache/other/page.tsx
@@ -1,8 +1,22 @@
+"use client"
+
+import { useEffect, useState } from "react"
 import { getRandomNumber } from "@/app/actions"
 import Link from "next/link"
 
-export default async function page() {
-  const number1 = await getRandomNumber()
+export const fetchCache = 'force-cache'
+
+export default function Page() {
+  const [number1, setNumber1] = useState<number | null>(null)
+
+  useEffect(() => {
+    const fetchNumber = async () => {
+      const num = await getRandomNumber()
+      setNumber1(num)
+    }
+
+    fetchNumber()
+  }, [])
 
   return (
     <>
